ecff33a38964b0c0edd4490cf32cc767dbb24130
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2023-10-09 23:42:23 +0530

    Time: 0 ms (100%), Space: 6.5 MB (8.87%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fn(int n , vector<int>&dp){
+        if(n==0) return 0;
+        if(n==1)return 1;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]= fn(n-1,dp)+ fn(n-2,dp);
+    }
+    int fib(int n) {
+    vector<int>dp(n+1,-1);
+    return fn(n,dp);
+    }
+   
+};
