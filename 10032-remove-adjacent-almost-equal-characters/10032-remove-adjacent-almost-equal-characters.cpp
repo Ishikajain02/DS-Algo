3570b8df0bbc57a295b07ece85750cfebd3239f6
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2023-12-09 21:37:08 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (100.00%) - LeetHub

---
+++ 10032-remove-adjacent-almost-equal-characters/10032-remove-adjacent-almost-equal-characters.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeAlmostEqualCharacters(string word) {
+        int count=0;
+        for(int i=0;i<word.length()-1;i++){
+            
+            if (abs(word[i]-word[i+1])<=1){
+                count++;
+                i++;
+            }
+        }
+            return count;
+    }
+};
