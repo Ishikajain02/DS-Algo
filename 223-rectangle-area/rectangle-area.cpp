8eadf5334f6a41cda054c3d64b4caef50a9dca05
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-02-29 14:51:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (45.00%) - LeetSync

--- 223-rectangle-area/rectangle-area.cpp
+++ 223-rectangle-area/rectangle-area.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
+bool overlap(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2){
+   if(ax1<bx2 && ay1<by2 && bx1<ax2 && by1<ay2)return true;
+   return false;
+}
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int a1=(ax2-ax1)*(ay2-ay1);
-        int b1=(bx2-bx1)*(by2-by1);
-        if(ay1>=by2 || ay2<=by1 || ax2<=bx1 || bx2<=ax1)return a1+b1;
-     //   if(a1==0)return b1;
-       // if(b1==0)return a1;
-        int xo= min(bx2,ax2)-max(bx1,ax1);
-        int yo=min(by2,ay2)-max(by1,ay1);
-        return a1+b1-(xo*yo);
+        int area1= (ax2-ax1)*(ay2-ay1);
+        int area2 = (bx2-bx1)*(by2-by1);
+        if(!overlap(ax1,ay1,ax2,ay2,bx1,by1,bx2,by2)){
+            return area1+area2;
+        }
+        else{
+            int diff = (min(bx2,ax2)-max(ax1,bx1))*(min(ay2,by2)-max(by1,ay1));
+            return area1+area2-diff;
+        }
+        return 0;
     }
 };
