69d652a11b9620ed1ab752af29cff888e50dd222
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-03-05 14:48:58 +0530

    Time: 58 ms (76.74%) | Memory: 20.3 MB (64.65%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     bool canChange(string start, string target) {
-        stack<pair<char,int>>st;
-        stack<pair<char,int>>st2;
-        for(int i=0;i<start.size();i++){
-          if(start[i]!='_')st.push({start[i],i});
-        }
-        for(int i=0;i<target.size();i++){
-            if(target[i]!='_')st2.push({target[i],i});
-        }
-        if(st.size()!=st2.size())return false;
-        while(!st.empty() && !st2.empty()){
-            if(st.top().first!=st2.top().first){
-                return false;
+        int i=0,j=0;
+        int n=start.size();
+        while(i<n || j<n){
+            while(i<n && start[i]=='_')i++;
+            while(j<n && target[j]=='_')j++;
+            if(i==n ||j==n)break;
+            if(start[i]==target[j]){
+                if(start[i]=='L' && i<j )return false;
+                else if (start[i]=='R' && i>j)return false;
+              
             }
-            else{
-                if(st.top().first=='L' && st.top().second<st2.top().second){
-                    return false;
-                }
-                else if(st.top().first=='R' && st.top().second>st2.top().second)return false;
-            }
-            st.pop();st2.pop();
+              else return false;
+                i++;
+                j++;
         }
-        return true;
+        if(i==n &&j==n)return true;
+        return false;
     }
 };
