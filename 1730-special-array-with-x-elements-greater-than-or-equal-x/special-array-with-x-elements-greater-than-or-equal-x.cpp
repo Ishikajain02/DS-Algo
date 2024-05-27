22d827ae149da92bb163fd61e947f714726bb127
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-05-27 14:57:01 +0530

    Time: 7 ms (5.29%) | Memory: 11.9 MB (5.12%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    int specialArray(vector<int>& nums) {
+/*    int specialArray(vector<int>& nums) {
+        //BRUTE FORCE
+        
+        TC-O(N^2);
+        SC-O(1);
         sort(nums.begin(),nums.end());
         for(int j=0;j<=nums[nums.size()-1];j++){
             int cnt=0;
@@ -12,5 +16,29 @@ public:
            }
         }
         return -1;
+    }*/
+    int search(vector<int>nums,int index){
+        int start=0;
+        int end=nums.size()-1;
+        int val=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]>=index){
+                val=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return val;
+    }
+    int specialArray(vector<int>nums){
+       sort(nums.begin(),nums.end());
+       for(int i=1;i<=nums.size();i++){
+        int val=search(nums,i);
+        if(i==(nums.size()-val))return i;
+       }
+       return -1;
     }
 };
