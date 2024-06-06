5ceff07ac1a0207a3816c60ea5b235dd5f60bcfd
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-06-06 15:07:40 +0530

    Time: 75 ms (18.82%) | Memory: 30.5 MB (78.12%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
+      //  sort(hand.begin(),hand.end());
         map<int,int>mp;
-        for(int i=0;i<hand.size();i++){
-            mp[hand[i]]++;
-        }
+        for(int i=0;i<hand.size();i++)mp[hand[i]]++;
         for(auto it=mp.begin();it!=mp.end();){
             if(it->second>0){
-                
-                    for(int i=0;i<groupSize;i++){
-                        if(mp[it->first+i]>0){
-                        mp[it->first+i]--;
-                        }
-                        else return false;
+                for(int j=0;j<groupSize;j++){
+                    if(mp[it->first+j]>0){
+                        mp[it->first+j]--;
                     }
+                    else return false;
                 }
-                   else it++; 
             }
-      return true;
+            else it++;
+        }
+        return true;
     }
 };
