64ec150985b846ea37bfef3ce5f835176cce07c9
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-06-10 21:08:44 +0530

    Time: 4 ms (50.00%) | Memory: 7.5 MB (83.33%) - LeetSync

---
+++ 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfChild(int n, int k) {
+        n--;
+        int round= k/n;
+        int rem=k%n;
+        if(round%2==0)return rem;
+        return n-rem;
+        
+    }
+};
