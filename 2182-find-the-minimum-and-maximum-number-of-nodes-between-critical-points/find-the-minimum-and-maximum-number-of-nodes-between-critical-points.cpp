528f918371cdeb40807a37471a5693e3a6d39312
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-07-05 11:54:08 +0530

    Time: 186 ms (11.08%) | Memory: 115.9 MB (58.25%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -22,29 +22,46 @@ int fn(ListNode*head){
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
      int localmini=INT_MAX,localmaxi=INT_MAX;
      ListNode* temp=head;
-     vector<int>ans;
+    // vector<int>ans;
      int size=fn(head);
      int cnt=1;
+     int firstmini=-1;
      if(size==2)return {-1,-1};
      temp=temp->next;
      ListNode*prev=head;
+     int prevnode=-1;
+     //int curr=-1;
      while(temp&& temp->next){
         cnt++;
       if(temp->val>prev->val && temp->val>temp->next->val){
-        ans.push_back(cnt);
+        if(firstmini==-1){
+            firstmini=cnt;
+            prevnode=firstmini;
+        }
+        else{
+          localmini=min(localmini,cnt-prevnode);
+          prevnode=cnt;
+        }
         
       }
       else if(temp->val<prev->val && temp->val<temp->next->val){
-        ans.push_back(cnt);
+        if(firstmini==-1){
+             firstmini=cnt;
+            prevnode=firstmini;
+        }
+        //ans.push_back(cnt);
+         else{
+          localmini=min(localmini,cnt-prevnode);
+          prevnode=cnt;
+        }
       }
       temp=temp->next;
       prev=prev->next;
      }   
-     if(ans.size()<2)return {-1,-1};
-     localmaxi=ans[ans.size()-1]-ans[0];
-    for(int i=0;i<ans.size()-1;i++){
-        localmini=min(localmini,ans[i+1]-ans[i]);
-    }
+     //if(size<2)return {-1,-1};
+ 
+    localmaxi=prevnode-firstmini;
+       if(localmini==INT_MAX || localmaxi==INT_MIN)return {-1,-1};
     return {localmini,localmaxi};
     }
 };
