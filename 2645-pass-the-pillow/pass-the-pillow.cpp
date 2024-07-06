eefee31dcae9406af16693e2ca0d3fe282260959
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-07-06 22:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (71.18%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-     //1 round=n-1
-     //2 round n-2
-      /* if(n>time)return time+1;
-      /*  n-2 divide
-        5/2
-        2
-        2/1
-        return ceil(time/(n-2));*/
-        bool fit=false;
-        int ind=0;
-        while(time!=0){
-            if(fit==false){
-                for(int i=2;i<=n && time>0;i++){
-                    
-                    time--;
-                    ind=i;
-                }
-                fit=true;
-            }
-            else{
-                for(int i=n;i>=2 && time>0 ;i--){
-                    time--;
-                    ind=i-1;
-                }
-                fit=false;
-            }
-
+        int remain=time%(n-1);
+        int round=time/(n-1);
+        if(round%2==0){
+            return remain+1;
         }
-        return ind;
+        else{
+            return n-remain;
+        }
     }
 };
