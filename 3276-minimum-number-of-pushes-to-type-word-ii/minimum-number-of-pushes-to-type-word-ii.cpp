059910e43fd89b1f1838809120a698c3b41f1650
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-08-06 14:39:59 +0530

    Time: 75 ms (51.16%) | Memory: 27.8 MB (14.43%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int>a(26,0);
-        for(int i=0;i<word.length();i++){
-            a[word[i]-'a']++;
+        vector<int>vc(26,0);
+        for(int i=0;i<word.size();i++){
+            vc[word[i]-'a']++;
         }
-        sort(a.begin(),a.end());
+        sort(vc.rbegin(),vc.rend());
         int ans=0;
-        for(int i=25,cnt=1;i>=0&& cnt<=26;i--,cnt++){
-            if(cnt<=8){
-                ans+=a[i];
-            }
-            else if (cnt>8 && cnt<=16){
-                int t = a[i]*2;
-                ans+=t;
-            }
-            else if (cnt>16 && cnt<=24){
-                int h = a[i]*3;
-                ans+=h;
-            }
-            else if (cnt>24 && cnt<=26){
-                int y = a[i]*4;
-                ans+=y;
+        int cnt=1;
+        int val=0;
+        for(int i=0;i<26;i++){
+            if(vc[i]>0){
+                val++;
+                ans+=(vc[i]*cnt);
+                if(val%8==0)cnt++;
             }
         }
         return ans;
