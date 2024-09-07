2abafebbf382cd9ce06adf5714b2dc76dfb992ad
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-09-07 14:27:22 +0530

    Time: 18 ms (92.91%) | Memory: 30.7 MB (87.87%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool travel(ListNode*temp,TreeNode*root){
+        if(!temp)return true;
+        if(!root)return false;
+        bool result=false;
+        if(root->val==temp->val){
+          result|=travel(temp->next,root->left);
+          result|=travel(temp->next,root->right);
+            
+        }
+        return result;
+    }
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if(!root)return false;
+        bool result=travel(head,root);
+        result|=isSubPath(head,root->left);
+        result|=isSubPath(head,root->right);
+        return result;
+    }
+};
