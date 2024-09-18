4cfc4e996047c52a4cff7bec7ff0d196bde536f9
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-09-19 01:50:54 +0530

    Time: 82 ms (56.88%) | Memory: 13.2 MB (53.82%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-  int recurr(int i,vector<int>&nums,int j,vector<vector<int>>&vc){
-    if(i>=nums.size() )return 0;
-    int take=0;
-    if(vc[i][j+1]!=-1)return vc[i][j+1];
-    if( j==-1 || nums[i]>nums[j]){
-        take=1+recurr(i+1,nums,i,vc);
-    }
-    int nottake=recurr(i+1,nums,j,vc);
-    return vc[i][j+1]= max(take,nottake);
-  }
     int lengthOfLIS(vector<int>& nums) {
-       //lis
-       //recursion take it or leave it
-       //memoise it
-       vector<vector<int>>vc(nums.size(),vector<int>(nums.size()+1,-1));
-       
-       return recurr(0,nums,-1,vc);
+        //tabulation 1 d array
+        int maxi=1;
+        vector<int>dp(nums.size(),1);
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+            }
+            maxi=max(maxi,dp[i]);
+        }
+        return maxi;
     }
 };
