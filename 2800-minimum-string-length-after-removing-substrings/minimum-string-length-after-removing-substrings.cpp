a7f0f7e7a2c7d9d4bd99c8dfb488390470d06853
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-10-07 20:29:53 +0530

    Time: 7 ms (51.96%) | Memory: 10.1 MB (93.92%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,39 +1,14 @@
 class Solution {
 public:
     int minLength(string s) {
-        stack<char>st;
-        char prev;
-        prev=s[0];
-        st.push(s[0]);
-        for(int i=1;i<s.size();i++){
-            cout<<st.size()<<endl;
-            if(s[i]=='B'){
-                if(prev=='A'){
-                    st.pop();
-                    if(!st.empty())prev=st.top();
-                    else prev='\0';
-                }
-                else {
-                    st.push(s[i]);
-                    prev=s[i];
-                }
+        int left=0;
+        for(int right=0;right<s.size();right++){
+            s[left]=s[right];
+            if(left>0 && (s[left-1]=='A' || s[left-1]=='C') && s[left]-'A'==s[left-1]-'A'+1){
+                left--;
             }
-            else if(s[i]=='D'){
-                if(prev=='C'){
-                    st.pop();
-                     if(!st.empty())prev=st.top();
-                    else prev='\0';
-                }
-                else {
-                    st.push(s[i]);
-                    prev=s[i];
-                }
-            }
-            else {
-                st.push(s[i]);
-                prev=s[i];
-            }
+            else left++;
         }
-        return st.size();
+        return left;
     }
 };
