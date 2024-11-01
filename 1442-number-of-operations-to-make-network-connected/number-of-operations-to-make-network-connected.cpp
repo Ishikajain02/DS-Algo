4ab48767745200eb43fe3cfcb994d14968b3f8ec
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-11-02 02:24:48 +0530

    Time: 2 ms (91.84%) | Memory: 41.6 MB (83.15%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
-    void dfs(vector<bool>&vis,int i,vector<vector<int>>&adj){
-        vis[i]=true;
-        for(int j=0;j<adj[i].size();j++){
-            if(!vis[adj[i][j]])dfs(vis,adj[i][j],adj);
+    int find(int node, vector<int>&parent){
+        if(parent[node]==node)return node;
+        return parent[node]=find(parent[node],parent);
+    }
+    void unioni(int x,int y,vector<int>&parent,vector<int>&rank){
+        int parent1=find(x,parent);
+        int parent2=find(y,parent);
+        if(parent1==parent2)return;
+        if(rank[parent1]==rank[parent2]){
+            parent[parent2]=parent1;
+            rank[parent1]++;
+
         }
-        return;
+        else if(rank[parent1]>rank[parent2]){
+            parent[parent2]=parent1;
+        }
+        else parent[parent1]=parent2;
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1)return -1;
-        int count=0;
-        vector<vector<int>>adj(n);
-        for(int i=0;i<connections.size();i++){
-            adj[connections[i][0]].push_back(connections[i][1]);
-            adj[connections[i][1]].push_back(connections[i][0]);
-        }
-        vector<bool>vis(n,false);
-        for(int i=0;i<n;i++){
-         if(!vis[i]){
-            dfs(vis,i,adj);
-            count++;
-         }
-        }
-        return count-1;
+        //count extra edges
+        //counting extra cables and filling parent array
+          if(connections.size()<n-1)return -1;
+       vector<int>parent(n,0);
+       vector<int>rank(n,1);
+       for(int i=0;i<n;i++){
+        parent[i]=i;
+       }
+     
+       int edges=0;
+       for(int i=0;i<connections.size();i++){
+         int parent1=find(connections[i][0],parent);
+         int parent2=find(connections[i][1],parent);
+         if(parent1==parent2)edges++;
+         else unioni(parent1,parent2,parent,rank);
+       }
+       int cnt=0;
+       for(int i=0;i<n;i++){
+       if(parent[i]==i)cnt++;
+       }
+       if(cnt-1<=edges)return cnt-1;
+       else return -1;
     }
 };
