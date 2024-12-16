bbace3cb93fbfae43990b247dabdd50061e6d5f6
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2024-12-16 15:25:46 +0530

    Time: 3 ms (30.95%) | Memory: 27.7 MB (29.75%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        //brute force
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(int i=0;i<nums.size();i++)pq.push({nums[i],i});
         while(k--){
-            int mini=INT_MAX;
-            int ind=-1;
-            for(int i=0;i<nums.size();i++){
-               if(mini>nums[i]){
-                 mini=min(mini,nums[i]);
-                 ind=i;
-               }
-            }
-            nums[ind]=nums[ind]*multiplier;
-
+   int f=pq.top().first;
+  int ind=pq.top().second;
+  f*=multiplier;
+  pq.pop();
+  pq.push({f,ind});
+        }
+        while(!pq.empty()){
+            nums[pq.top().second]=pq.top().first;
+            pq.pop();
         }
         return nums;
     }
