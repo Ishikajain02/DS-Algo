a013945e0f6adba61eee2a08313cad0a0ca6b7d4
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2025-01-08 06:04:43 +0530

    Time: 2 ms (72.03%) | Memory: 23.4 MB (78.18%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int countPrefixSuffixPairs(vector<string>& words) {
-        bool t1=false,t2=false;
-      //  sort(words.begin(),words.end());
-        int cnt=0;
-        for(int i=0;i<words.size()-1;i++){
-            int size=words[i].size();
+        int ans=0;
+        for(int i=0;i<words.size();i++){
             for(int j=i+1;j<words.size();j++){
-                int sj=words[j].size();
-              //  if(sj<size)break;
-                if(words[j].substr(0,size)==words[i] && words[j].substr(sj-size,size)==words[i]){
-                    cnt++;
+                if(words[i].size()>words[j].size())continue;
+                else{
+                  string sub1=words[j].substr(0,words[i].size());
+                  string substr2=words[j].substr(words[j].size()-words[i].size());
+                  if(sub1==substr2 && sub1==words[i])ans++;
                 }
             }
         }
-        return cnt;
+        return ans;
     }
 };
