3ee3e818282f4e7a6ebf4783eaaee7af8da0e5cc
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2025-07-14 00:02:31 +0530

    Time: 31 ms (43.41%) | Memory: 80.1 MB (97.54%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-        sort(trainers.begin(),trainers.end());
-        int left=0;
         sort(players.begin(),players.end());
-        int right=0,ans=0;
-        while(right<players.size() && left<trainers.size()){
-         if(trainers[left]>=players[right]){
-            ans++;
-            left++;
-            right++;
-         }
-         else left++;
-
+        sort(trainers.begin(),trainers.end());
+        int i=0,j=0;
+        int ans=0;
+        while(i<players.size() && j<trainers.size()){
+            if(players[i]<=trainers[j]){
+                ans++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
         }
         return ans;
     }
