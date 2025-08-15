9884f818e3b7d392a7059dd5bb025e6374192eb4
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2025-08-15 23:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (83.54%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0 || n<0)return false;
+        int count = __builtin_popcount(n);
+if(count>1)return false;
+return true;
+    }
+};
