430b5e941654bf4bd626a1d033091e4f16e69206
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2025-09-01 01:04:59 +0530

    Time: 751 ms (5.10%) | Memory: 118.2 MB (15.17%) - LeetSync

---
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+           vector<int> stack;
+        int nums_to_delete = nums.size()-k;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!stack.empty() && nums[i] < stack.back() && nums_to_delete) {
+                cout<<stack.back()<<endl;
+                stack.pop_back();
+                nums_to_delete--;
+            }
+            stack.push_back(nums[i]);
+        }
+        return vector<int>(stack.begin(), stack.begin()+k);
+    }
+};
