3ad6b8c1183a9526e57884efdee6a5365128f166
Ishika Jain <94300478+Ishikajain02@users.noreply.github.com>
2025-12-06 03:20:53 +0530

    Time: 3 ms (8.46%) | Memory: 23.4 MB (5.26%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int countPartitions(vector<int>& nums) {
-        //count the number of partitions where the difference between sum of left and right subrray  is even
-        int ans=0;
-        vector<int>prefix(nums.size(),0);
+        int n=nums.size();
+        vector<int>prefix(n,0);
+        vector<int>suffix(n,0);
         prefix[0]=nums[0];
-        for(int i=1;i<nums.size();i++){
-          prefix[i]=prefix[i-1]+nums[i];
+        suffix[n-1]=nums[n-1];
+        for(int i=1;i<n;i++)prefix[i]=prefix[i-1]+nums[i];
+        for(int j=n-2;j>=0;j--){
+            suffix[j]=suffix[j+1]+nums[j];
         }
-        for(int i=0;i<nums.size()-1;i++){
-            if(((prefix[nums.size()-1]-prefix[i])-prefix[i])%2==0)ans++;
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            if(abs(prefix[i]-suffix[i+1])%2==0)ans++;
         }
         return ans;
     }
